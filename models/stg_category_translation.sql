3485496f5f929b59fc011fdd66e3e5dd4dea3a47
yuzuan419656 <yuzuan419656@gmail.com>
2026-07-08 11:58:37 +0900

    Fix SQL syntax for category translation query

--- models/stg_category_translation.sql
+++ models/stg_category_translation.sql
@@ -1,4 +1,4 @@
-select
-    col0 as product_category_name,
-    col1 as product_category_name_english
-from {{ source('olist_source', 'raw_product_category_name_translation') }}
+SELECT
+    col0 AS product_category_name,
+    col1 AS product_category_name_english
+FROM {{ source('olist_source', 'raw_product_category_name_translation') }}
