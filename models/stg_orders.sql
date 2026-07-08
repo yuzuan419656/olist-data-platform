f10703d5da702e8bd7c66b111297276887fb5034
yuzuan419656 <yuzuan419656@gmail.com>
2026-07-08 11:57:35 +0900

    Expand SQL selection to include additional order fields

--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -1,6 +1,10 @@
-select
-    order_id,
-    customer_id,
-    order_status,
-    order_purchase_timestamp
-from {{ source('olist_source', 'raw_orders') }}
+SELECT
+    col0 AS order_id,
+    col1 AS customer_id,
+    col2 AS order_status,
+    col3 AS order_purchase_timestamp,
+    col4 AS order_approved_at,
+    col5 AS order_delivered_carrier_date,
+    col6 AS order_delivered_customer_date,
+    col7 AS order_estimated_delivery_date
+FROM {{ source('olist_source', 'raw_orders') }}
